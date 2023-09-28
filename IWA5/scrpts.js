3258543391ff1d4a0b3c20f41fa8f9c921243354
thembaledwaba <140729731+thembaledwaba@users.noreply.github.com>
2023-09-28 19:05:28 +0200

    Add files via upload

---
+++ IWA5/scrpts.js
@@ -0,0 +1,49 @@
+let warningText = 'Free shipping only applies to single customer orders'
+const banned = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = 0
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * NONE_SELECTED
+let batteries = 35 * 2
+let pens = 5 * NONE_SELECTED
+
+let shipping = 400
+let currency = 'R'
+
+let destination = 'RSA'
+if (destination === 'RSA') { shipping === 400 && currency === 'R' }
+console.log(location)
+
+destination = 'NAM'
+if (destination === 'NAM') {
+	(shipping === 600) 
+	}
+	else 
+	{shipping === 800}
+
+
+const totalPrice= shoes + batteries + pens + shirts + toys
+if (totalPrice >= 1000 && 60) {
+	if (destination === NAM && customers < 2) {
+			if (location === RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+
+if (shipping = 0 && customers !== 1) 
+{ console.log(warningText) }
+
+if (destination === 'NK') { console.log(banned)}
+
+
+customers = 1
+destination = 'RSA'
+currency = "R"
+shipping = 400
+if (destination === "RSA" && customers === 1){
+    console.log('Price:',currency, totalPrice + shipping)
+}
+
+
+
